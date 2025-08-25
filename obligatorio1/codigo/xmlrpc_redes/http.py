753baf333415a03b9ccded9390bb39f03ad945fa
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-08-24 21:31:23 -0300

    Cambio en construccion de http headers: - Se deja la contruccion en el mismo formato para el req y res - Se agrega User-Agent a res

--- obligatorio1/codigo/xmlrpc_redes/http.py
+++ obligatorio1/codigo/xmlrpc_redes/http.py
@@ -17,11 +17,18 @@ def build_http_request(host, port, body_bytes):
         f"Host: {host}:{port}\r\n"
         f"User-Agent: MiClienteXMLRPC/1.0\r\n"
         f"Content-Type: text/xml\r\n"
-        f"Content-Length: {len(body_bytes)}\r\n\r\n"
+        f"Content-Length: {len(body_bytes)}\r\n"
+        f"\r\n"
     ).encode('utf-8')
     return req + body_bytes
 
 def build_http_response(body_bytes, status=200, status_text='OK'):
     # Construye una respuesta HTTP para XML-RPC
-    h = f'HTTP/1.1 {status} {status_text}\r\nContent-Length: {len(body_bytes)}\r\nContent-Type: text/xml\r\n\r\n'
-    return h.encode() + body_bytes
+    req = (
+        f"HTTP/1.1 {status} {status_text}\r\n"
+        f"User-Agent: MiServidorXMLRPC/1.0\r\n"
+        f"Content-Type: text/xml\r\n"
+        f"Content-Length: {len(body_bytes)}\r\n"
+        f"\r\n"
+    ).encode('utf-8')
+    return req + body_bytes
